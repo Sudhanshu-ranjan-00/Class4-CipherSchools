568872907d5c4d434be4addddebec8d436ac5d68
Sudhanshu Ranjan <rajaashujha@gmail.com>
2023-06-25 15:59:34 +0530

    Class4-CipherSchools

---
+++ primitiveDT.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class primitiveDT {
+    public static void main(String args[]) {
+        int x = 5;
+        int y = 7;
+        System.out.println("x is : " + x);
+        System.out.println("y is : " + y);
+        byte B1 = (byte) 130;
+        System.out.println("B1 is : " + B1);
+        byte b = 10;
+        System.out.println("B is : " + b);
+        int i = b;
+        System.out.println("I is : " + i);
+
+    }
+
+}
